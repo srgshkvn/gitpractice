f196e1dc87bfa41d7eefafb98ed216d1949a79c1
Nursultan Sargashkaev <sargashkaevn@Nursultans-MacBook-Pro.local>
2020-07-14 03:30:14 -0500

    I have added functionality

---
+++ src/git/practice3.java
@@ -0,0 +1,17 @@
+package git;
+
+public class practice3 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(getSum());
+
+
+
+    }
+
+    public static int getSum(){
+        return 5+5;
+    }
+}
