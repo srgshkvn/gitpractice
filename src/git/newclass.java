944495cc6afc5ad88eb6093bdf447f521a4c13bb
Nursultan Sargashkaev <sargashkaevn@Nursultans-MacBook-Pro.local>
2020-07-14 13:36:06 -0500

    created a new file

---
+++ src/git/newclass.java
@@ -0,0 +1,14 @@
+package git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class newclass {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Created a new class");
+    }
+}
