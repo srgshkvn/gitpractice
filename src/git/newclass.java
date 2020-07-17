3ad492afff813d0d2a8dbacf85762e66b85fa4eb
Nursultan Sargashkaev <sargashkaevn@Nursultans-MacBook-Pro.local>
2020-07-17 16:16:30 -0500

    created a new class

--- src/git/newclass.java
+++ src/git/newclass.java
@@ -12,5 +12,22 @@ public class newclass {
         System.out.println("Created a new class");
 
 
+
+
+//        Git pull origin master - to pull the latest changes from remote repo
+//        Git branch - to list all active branches
+//        git checkout [name of the branch] - to switch the branch you are working on
+//        git checkout -b [name of the branch] - create and switch to new branch
+//        pull request - is a request to master to pull changes from your branch
+//        merge - is submitting your changes into master branch
+//        Good practise is to always pull latest changes before you push your changes
+//        pull request is a method of submitting contributions(changes) to a project.
+//        merge -  lets you integreate changes into a single branch [git merge]
+//        Conflict - arises when two separate branches have made changes to the same line in the same file.
+//                RULE - Pull before you push!
+//                RULE - Always start with pulling hte latest changes
+//        git restore [file path] to restore the file state to preveios commit
+
+
     }
 }
